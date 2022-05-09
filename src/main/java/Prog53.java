f251a1b317790668554a175f6122dd5841b04aef
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-09 21:53:05 +0900

    Update Prog53.java

--- src/main/java/Prog53.java
+++ src/main/java/Prog53.java
@@ -1,8 +1,8 @@
 public class Prog53 {
 
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
 
-	}
+    }
 }
 
